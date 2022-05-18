c79443339c80030f039f0bb4210fbd13b5633aaf
wid_ds_training <wid_ds_training@QAW400>
2022-05-18 16:51:03 +0900

    「getAllMessagesを追加」

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,57 +7,73 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-//DTO
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
-@Table(name="message")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
+@Table(name = "messages")
 public class Message {
-	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	@Column(name="content",length=255,nullable=false)
-	private String content;
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
 
-	@Column(name="created_at",nullable=false)
-	private Timestamp created_at;
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
 
-	@Column(name="update_at",nullable=false)
-	private Timestamp update_at;
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
 
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public Timestamp getCreated_at() {
-		return created_at;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setCreated_at(Timestamp created_at) {
-		this.created_at = created_at;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public Timestamp getUpdate_at() {
-		return update_at;
-	}
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
 
-	public void setUpdate_at(Timestamp update_at) {
-		this.update_at = update_at;
-	}
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
 
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
 }
+
