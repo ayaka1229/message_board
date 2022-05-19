5146eefc823f6bd472308c890ea32e456cc30f84
wid_ds_training <wid_ds_training@QAW400>
2022-05-19 14:41:38 +0900

    「存在しないidに対する処理を追加」

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -44,8 +44,11 @@ public class EditServlet extends HttpServlet {
 		request.setAttribute("_token", request.getSession().getId());
 
 
+		//	メッセージデータが存在しているときのみ
 		//メッセージIDをセッションスコープに登録(updateへの受け渡しで必要)
-		request.getSession().setAttribute("message_id", m.getId());
+		if(m != null) {
+			request.getSession().setAttribute("message_id", m.getId());
+		}
 
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
 		rd.forward(request, response);
